5447492fc306d92cafeae26a220d4e585f6fbf26
wangpeiyuan <490176245@qq.com>
2022-09-02 17:02:52 +0800

    添加 get请求含header头、delete和put请求封装

--- src/main/java/com/github/dc/invoke/helper/RestTemplateHelper.java
+++ src/main/java/com/github/dc/invoke/helper/RestTemplateHelper.java
@@ -4,12 +4,10 @@ import com.github.dc.invoke.util.ApiLogSetupHelper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
@@ -42,12 +40,35 @@ public class RestTemplateHelper {
     }
 
     /**
+     * get请求
+     * @param url
+     * @param headers
+     * @param responseType
+     * @return
+     */
+    public <R> ResponseEntity<R> get(String url, HttpHeaders headers, Class<R> responseType) {
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(headers);
+        return dcRestTemplate.exchange(url, HttpMethod.GET, httpEntity, responseType);
+    }
+
+    /**
      * 根据下载链接获取文件字节数组
      * @param url
      * @return
      */
     public ResponseEntity<byte[]> getBytesForEntity(String url) {
         return dcRestTemplate.getForEntity(url, byte[].class);
+    }
+
+    /**
+     * 根据下载链接获取文件字节数组
+     * @param url
+     * @param headers
+     * @return
+     */
+    public ResponseEntity<byte[]> getBytesForEntity(String url, HttpHeaders headers) {
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(headers);
+        return dcRestTemplate.exchange(url, HttpMethod.GET, httpEntity, byte[].class);
     }
 
     /**
@@ -65,6 +86,21 @@ public class RestTemplateHelper {
     }
 
     /**
+     * 根据下载链接获取文件字节数组
+     * @param url
+     * @param headers
+     * @return
+     */
+    public byte[] getBytes(String url, HttpHeaders headers) {
+        ResponseEntity<byte[]> response = this.getBytesForEntity(url, headers);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            return response.getBody();
+        }
+        log.warn("下载资源失败，GET {}，response: {}", url, response);
+        return null;
+    }
+
+    /**
      * 根据下载链接获取文件字节数组并写入输出流
      * @param url
      * @param os
@@ -72,6 +108,20 @@ public class RestTemplateHelper {
     public void getBytesToOutputStream(String url, OutputStream os) {
         try {
             os.write(this.getBytes(url));
+        } catch (IOException e) {
+            log.warn("下载资源失败，GET {}" + url, e);
+        }
+    }
+
+    /**
+     * 根据下载链接获取文件字节数组并写入输出流
+     * @param url
+     * @param headers
+     * @param os
+     */
+    public void getBytesToOutputStream(String url, HttpHeaders headers, OutputStream os) {
+        try {
+            os.write(this.getBytes(url, headers));
         } catch (IOException e) {
             log.warn("下载资源失败，GET {}" + url, e);
         }
@@ -219,5 +269,202 @@ public class RestTemplateHelper {
                 .headers(headers)
                 .body(body);
         return dcRestTemplate.postForEntity(url, requestEntity, responseType);
+    }
+
+    /**
+     * delete请求
+     * @param url 请求地址
+     */
+    public void delete(String url) {
+        dcRestTemplate.delete(url);
+    }
+
+    /**
+     * delete请求
+     * @param url 请求地址
+     * @param headers 请求头
+     * @return 返回内容
+     */
+    public void delete(String url, HttpHeaders headers) {
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(headers);
+        dcRestTemplate.exchange(url, HttpMethod.DELETE, httpEntity, void.class);
+    }
+
+    /**
+     * delete请求
+     * @param url 请求地址
+     * @param responseType 返回类型
+     * @param <R>
+     * @return 返回内容
+     */
+    public <R> ResponseEntity<R> delete(String url, Class<R> responseType) {
+        RequestEntity requestEntity = RequestEntity
+                .delete(url)
+                .accept(MediaType.ALL)
+                .acceptCharset(StandardCharsets.UTF_8)
+                .build();
+        return dcRestTemplate.exchange(requestEntity, responseType);
+    }
+
+    /**
+     * delete请求
+     * @param url 请求地址
+     * @param headers 请求头
+     * @param responseType 返回类型
+     * @param <R>
+     * @return 返回内容
+     */
+    public <R> ResponseEntity<R> delete(String url, HttpHeaders headers, Class<R> responseType) {
+        RequestEntity requestEntity = RequestEntity
+                .delete(url)
+                .accept(MediaType.ALL)
+                .acceptCharset(StandardCharsets.UTF_8)
+                .headers(headers)
+                .build();
+        return dcRestTemplate.exchange(requestEntity, responseType);
+    }
+
+    /**
+     * put x-www-form-urlencoded格式内容请求
+     * @param url 请求地址
+     * @param param 请求内容
+     * @param responseType 返回值类型
+     * @param <R>
+     * @return 返回内容
+     */
+    public <R> ResponseEntity<R> putFormUrlencoded(String url, LinkedMultiValueMap<String, Object> param, Class<R> responseType) {
+        return this.put(url, MediaType.APPLICATION_FORM_URLENCODED, null, param, responseType);
+    }
+
+    /**
+     * put x-www-form-urlencoded格式内容请求
+     * @param url 请求地址
+     * @param headers 请求头
+     * @param param 请求内容
+     * @param responseType 返回值类型
+     * @param <R>
+     * @return 返回内容
+     */
+    public <R> ResponseEntity<R> putFormUrlencoded(String url, HttpHeaders headers, LinkedMultiValueMap<String, Object> param, Class<R> responseType) {
+        return this.put(url, MediaType.APPLICATION_FORM_URLENCODED, headers, param, responseType);
+    }
+
+    /**
+     * put json格式内容请求
+     * @param url 请求地址
+     * @param bodyJson 请求内容json字符串
+     * @param responseType 返回值类型
+     * @param <R>
+     * @return 返回内容
+     */
+    public <R> ResponseEntity<R> putJson(String url, String bodyJson, Class<R> responseType) {
+        return this.put(url, MediaType.APPLICATION_JSON, null, bodyJson, responseType);
+    }
+
+    /**
+     * put json格式内容请求
+     * @param url 请求地址
+     * @param headers 请求头
+     * @param bodyJson 请求内容json字符串
+     * @param responseType 返回值类型
+     * @param <R>
+     * @return 返回内容
+     */
+    public <R> ResponseEntity<R> putJson(String url, HttpHeaders headers, String bodyJson, Class<R> responseType) {
+        return this.put(url, MediaType.APPLICATION_JSON, headers, bodyJson, responseType);
+    }
+
+    /**
+     * put form-data格式内容请求
+     * @param url 请求地址
+     * @param param 请求form内容
+     * @param responseType 返回值类型
+     * @param <R>
+     * @return 返回内容
+     */
+    public <R> ResponseEntity<R> putFormData(String url, LinkedMultiValueMap<String, Object> param, Class<R> responseType) {
+        return this.put(url, MediaType.parseMediaType("multipart/form-data; charset=UTF-8"), null, param, responseType);
+    }
+
+    /**
+     * put form-data格式内容请求
+     * @param url 请求地址
+     * @param headers 请求头
+     * @param param 请求form内容
+     * @param responseType 返回值类型
+     * @param <R>
+     * @return 返回内容
+     */
+    public <R> ResponseEntity<R> putFormData(String url, HttpHeaders headers, LinkedMultiValueMap<String, Object> param, Class<R> responseType) {
+        return this.put(url, MediaType.parseMediaType("multipart/form-data; charset=UTF-8"), headers, param, responseType);
+    }
+
+    /**
+     * put form-data格式内容请求
+     * @param url 请求地址
+     * @param param 请求form内容
+     * @param fileParamName 请求form的文件变量名
+     * @param file 请求form的文件
+     * @param responseType 返回值类型
+     * @param <R>
+     * @return 返回内容
+     */
+    public <R> ResponseEntity<R> putFormData(String url, LinkedMultiValueMap<String, Object> param, String fileParamName, File file, Class<R> responseType) {
+        FileSystemResource fileResource = new FileSystemResource(file);
+        param.add(fileParamName, fileResource);
+        return this.put(url, MediaType.parseMediaType("multipart/form-data; charset=UTF-8"), null, param, responseType);
+    }
+
+    /**
+     * put form-data格式内容请求
+     * @param url 请求地址
+     * @param headers 请求头
+     * @param param 请求form内容
+     * @param fileParamName 请求form的文件变量名
+     * @param file 请求form的文件
+     * @param responseType 返回值类型
+     * @param <R>
+     * @return 返回内容
+     */
+    public <R> ResponseEntity<R> putFormData(String url, HttpHeaders headers, LinkedMultiValueMap<String, Object> param, String fileParamName, File file, Class<R> responseType) {
+        FileSystemResource fileResource = new FileSystemResource(file);
+        param.add(fileParamName, fileResource);
+        return this.put(url, MediaType.parseMediaType("multipart/form-data; charset=UTF-8"), headers, param, responseType);
+    }
+
+    /**
+     * put请求
+     * @param url 请求地址
+     * @param contentType 请求contentType
+     * @param body 请求body 根据contenType不同，类型不同
+     * @param responseType 返回值类型
+     * @param <R>
+     * @param <B>
+     * @return 返回内容
+     */
+    public <R, B> ResponseEntity<R> put(String url, MediaType contentType, B body, Class<R> responseType) {
+        return this.put(url, contentType, null, body, responseType);
+    }
+
+    /**
+     * put请求
+     * @param url 请求地址
+     * @param contentType 请求contentType
+     * @param headers 请求头
+     * @param body 请求body 根据contenType不同，类型不同
+     * @param responseType 返回值类型
+     * @param <R>
+     * @param <B>
+     * @return 返回内容
+     */
+    public <R, B> ResponseEntity<R> put(String url, MediaType contentType, HttpHeaders headers, B body, Class<R> responseType) {
+        RequestEntity requestEntity = RequestEntity
+                .put(url)
+                .contentType(contentType)
+                .accept(MediaType.ALL)
+                .acceptCharset(StandardCharsets.UTF_8)
+                .headers(headers)
+                .body(body);
+        return dcRestTemplate.exchange(requestEntity, responseType);
     }
 }
